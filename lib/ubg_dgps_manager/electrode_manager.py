48da4d6eef0b488f3b05528f1be2e3ead8388ad3
Maximilian Weigand <mweigand@mweigand.net>
2024-12-06 14:02:35 +0100

    electrode manager: add tools for shifting electrodes, as well as adding new elecs to fron/back

--- lib/ubg_dgps_manager/electrode_manager.py
+++ lib/ubg_dgps_manager/electrode_manager.py
@@ -122,6 +122,91 @@ class electrode_manager(object):
         # we render the whole manager in here
         self.output = output
 
+    def get_shift_elcs_widgets(self):
+        """Sometimes we want to shift a series of electrodes
+        """
+        shift_widgets = []
+
+        shift_widgets += [
+            widgets.Label('Shift electrodes'),
+            widgets.BoundedIntText(
+                value=0,
+                description='start at nr:',
+                style={'description_width': 'initial'},
+                disabled=False,
+                min=0,
+            ),
+            widgets.BoundedIntText(
+                value=0,
+                description='up to:',
+                style={'description_width': 'initial'},
+                disabled=False,
+            ),
+            widgets.FloatText(
+                value=0,
+                description='on the z-axis by [m]:',
+                style={'description_width': 'initial'},
+                disabled=False,
+            ),
+            widgets.Button(description='Shift on z-axis'),
+        ]
+        gbox = GridBox(
+            children=shift_widgets,
+            layout=Layout(
+                width='100%',
+                grid_template_columns='20% 20% 20% 20% 20%',
+                border='solid',
+            )
+        )
+
+        return shift_widgets, gbox
+
+    def get_add_new_elec_widgets(self):
+        """Sometimes we want to add new electrodes at the beginning or end
+        """
+        new_elec_widgets = []
+
+        new_elec_widgets += [
+            widgets.Label('Add new electrode at front:'),
+            widgets.FloatText(
+                value=0,
+                description='x:',
+                style={'description_width': 'initial'},
+                disabled=False,
+            ),
+            widgets.FloatText(
+                value=0,
+                description='z:',
+                style={'description_width': 'initial'},
+                disabled=False,
+            ),
+            widgets.Button(description='Add electrode'),
+            widgets.Label('Add new electrode at end:'),
+            widgets.FloatText(
+                value=0,
+                description='x:',
+                style={'description_width': 'initial'},
+                disabled=False,
+            ),
+            widgets.FloatText(
+                value=0,
+                description='z:',
+                style={'description_width': 'initial'},
+                disabled=False,
+            ),
+            widgets.Button(description='Add electrode'),
+        ]
+        gbox = GridBox(
+            children=new_elec_widgets,
+            layout=Layout(
+                width='100%',
+                grid_template_columns='25% 25% 25% 25%',
+                border='solid',
+            )
+        )
+
+        return new_elec_widgets, gbox
+
     def get_resampler_widgets(self):
         """The resampler interpolates all points between two electrodes and
         replaces all electrodes in this range with a certain Number of equally
@@ -179,6 +264,67 @@ class electrode_manager(object):
             )
         )
         return resample_widgets, gbox
+
+    def shift_electrodes(self, button):
+        start_elec = self.shift_widgets[1].value
+        end_elec = self.shift_widgets[2].value + 1
+        z_change = self.shift_widgets[3].value
+        assert start_elec >= 0, "Start electrode must be >= 0"
+        assert end_elec > start_elec, "end electrode must be > start electrode"
+        actives = np.where(self.electrode_positions[:, 3])[0]
+        print('actives', actives)
+        assert end_elec <= actives.size, 'end electrode too large'
+
+        print(
+            'Shifting electrodes from ' +
+            '{} to (including) {} by {} m on z-axis'.format(
+                start_elec,
+                end_elec,
+                z_change
+            )
+        )
+
+        # shift the z-axis
+        self.electrode_positions[actives[start_elec:end_elec], 2] += \
+            z_change
+        self._update_widgets()
+
+    def add_elc_front(self, button):
+        new_x = self.add_elc_widgets[1].value
+        new_z = self.add_elc_widgets[2].value
+        print(
+            'Adding new electrode with coordinates ({}/{}) at FRONT'.format(
+                new_x, new_z
+            )
+        )
+
+        self.electrode_positions = np.vstack((
+            np.array((new_x, 0, new_z, 1))[np.newaxis, :],
+            self.electrode_positions,
+        ))
+        # add new widget rows
+        row = self._get_electrode_widgets_row()
+        self.el_widgets += [row]
+        self.widgets['gridbox'].children += tuple(row)
+        self._update_widgets()
+
+    def add_elc_back(self, button):
+        new_x = self.add_elc_widgets[5].value
+        new_z = self.add_elc_widgets[6].value
+        print(
+            'Adding new electrode with coordinates ({}/{}) at BACK'.format(
+                new_x, new_z
+            )
+        )
+        self.electrode_positions = np.vstack((
+            self.electrode_positions,
+            np.array((new_x, 0, new_z, 1))[np.newaxis, :],
+        ))
+        # add new widget rows
+        row = self._get_electrode_widgets_row()
+        self.el_widgets += [row]
+        self.widgets['gridbox'].children += tuple(row)
+        self._update_widgets()
 
     def resample_points(self, button):
         start_electrode = self.resample_widgets[2].value
@@ -338,18 +484,32 @@ class electrode_manager(object):
         self.widgets['button_interp'].on_click(
             self.interpolate_between_points
         )
-        self.widgets['hbox_interp'] = widgets.HBox([
-            self.widgets['label_interp'],
-            self.widgets['int_interp_from'],
-            self.widgets['label_interp_to'],
-            self.widgets['int_interp_to'],
-            self.widgets['button_interp'],
-        ])
+        self.widgets['gbox_interp'] = GridBox(
+            children=[
+                self.widgets['label_interp'],
+                self.widgets['int_interp_from'],
+                self.widgets['label_interp_to'],
+                self.widgets['int_interp_to'],
+                self.widgets['button_interp'],
+            ],
+            layout=Layout(
+                width='100%',
+                grid_template_columns='20% 20% 20% 20% 20%',
+                border='solid',
+            )
+        )
 
         self.resample_widgets, resample_gbox = self.get_resampler_widgets()
         self.resample_widgets[5].on_click(
             self.resample_points
         )
+
+        self.add_elc_widgets, add_ne_gbox = self.get_add_new_elec_widgets()
+        self.add_elc_widgets[3].on_click(self.add_elc_front)
+        self.add_elc_widgets[7].on_click(self.add_elc_back)
+
+        self.shift_widgets, shift_gbox = self.get_shift_elcs_widgets()
+        self.shift_widgets[4].on_click(self.shift_electrodes)
 
         self.xz_header = [
             widgets.HTML('<b>El-Nr (1:)</b>'),
@@ -384,8 +544,10 @@ class electrode_manager(object):
 
         vbox = widgets.VBox([
             self.widgets['gridbox'],
-            self.widgets['hbox_interp'],
+            self.widgets['gbox_interp'],
             resample_gbox,
+            add_ne_gbox,
+            shift_gbox,
             self.widgets['output_points'],
             self.widgets['button_print'],
             self.widgets['output_print'],
@@ -455,7 +617,7 @@ class electrode_manager(object):
         self.widgets['output_points'].clear_output()
 
         with plt.ioff():
-            fig, ax = plt.subplots()
+            fig, ax = plt.subplots(figsize=(18 / 2.54, 10 / 2.54))
             for position in self.electrode_positions:
                 if position[3] == 1:
                     ax.scatter(
@@ -469,7 +631,7 @@ class electrode_manager(object):
             ax.set_title(
                 'Mesh topography (z-axis relative to lowest electrode)'
             )
-            ax.grid()
+            ax.grid(color='k')
 
         with self.widgets['output_points']:
             display(fig)
