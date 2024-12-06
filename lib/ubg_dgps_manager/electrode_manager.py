31836da3e8b20858cfc0142439719d27cdb3d14b
Maximilian Weigand <mweigand@mweigand.net>
2024-12-06 12:12:04 +0100

    add a resampling option in the electrode manager

--- lib/ubg_dgps_manager/electrode_manager.py
+++ lib/ubg_dgps_manager/electrode_manager.py
@@ -17,8 +17,82 @@ import ipywidgets as widgets
 from ipywidgets import GridBox, Layout
 import matplotlib.pyplot as plt
 from IPython.display import display
+from scipy.interpolate import PchipInterpolator
 
 from .log_helper import ListHandler
+
+# import matplotlib.pylab as plt
+
+
+def get_resampled_positions(data_x_raw, data_z_raw, requested_spacing):
+    """
+    Parameters
+    ----------
+    data_x: numpy.ndarray of size N
+        X-positions
+    data_z: numpy.ndarray of size N
+        Z-positions
+    requested_spacing: float
+        Requested spacing of electrodes
+
+    Returns
+    -------
+    x_new: numpy.ndarray
+        New x coordinates
+    z_new: numpy.ndarray
+        New z coordinates
+    N: int
+        Number of new electrodes
+    """
+    data_x = np.atleast_1d(data_x_raw)
+    data_z = np.atleast_1d(data_z_raw)
+
+    # compute distance between the input coordinates
+    xy_dist_orig = np.cumsum(
+        np.hstack(
+            (
+                0,
+                np.sqrt(np.diff(data_x) ** 2 + np.diff(data_z) ** 2)
+            )
+        )
+    )
+    max_dist_orig = xy_dist_orig.max()
+    N_approx = np.floor(max_dist_orig / requested_spacing)
+    print('N_approx:', N_approx)
+
+    interp = PchipInterpolator(data_x, data_z)
+    # evaluate the spline at 4 times the density of the approximate requested
+    # spacing
+    x_val = np.linspace(
+        data_x.min(), data_x.max(), int(np.ceil(N_approx * 4))
+    )
+    y_interp = interp(x_val)
+
+    xy_dist = np.cumsum(
+        np.hstack(
+            (
+                0,
+                np.sqrt(np.diff(x_val) ** 2 + np.diff(y_interp) ** 2)
+            )
+        )
+    )
+    print('xy_dist', xy_dist)
+    # this is the
+    max_dist = int(
+        requested_spacing * (int(xy_dist.max() / requested_spacing)) + 1
+    )
+    print('max_dist', max_dist)
+
+    # compute x values
+    s_reg = np.linspace(0, max_dist, int(max_dist / requested_spacing + 1))
+    print('s_reg', s_reg)
+
+    x_reg = np.interp(s_reg, xy_dist, x_val)
+    print('x_reg', x_reg)
+
+    y_reg = interp(x_reg)
+
+    return x_reg, y_reg, x_reg.size
 
 
 class electrode_manager(object):
@@ -34,6 +108,7 @@ class electrode_manager(object):
         self.el_coords_orig = electrode_positions
         self.electrode_positions = np.hstack((
             self.el_coords_orig[:, :],
+            # active/disabled column
             np.ones(electrode_positions.shape[0])[:, np.newaxis],
         ))
 
@@ -47,6 +122,123 @@ class electrode_manager(object):
         # we render the whole manager in here
         self.output = output
 
+    def get_resampler_widgets(self):
+        """The resampler interpolates all points between two electrodes and
+        replaces all electrodes in this range with a certain Number of equally
+        spaced electrodes (along the interpolated line).
+
+        This function generates the GUI elements of the resampler.
+
+        Returns
+        -------
+        resample_widgets: list
+            A list of gui widgets
+        gbox: ipywidgets.GridBox
+            The GridBox containing the widgets
+        """
+        resample_widgets = []
+
+        resample_widgets += [
+            widgets.Label(
+                'Replace Electrode range (including start and end):'
+            ),
+            # dummy to enforce a nicer layout
+            widgets.Label(''),
+            widgets.BoundedIntText(
+                value=0,
+                description='Start Electrode:',
+                style={'description_width': 'initial'},
+                disabled=False,
+                min=0,
+                max=self.electrode_positions.shape[0] - 1,
+            ),
+            widgets.BoundedIntText(
+                value=0,
+                description='End Electrode:',
+                style={'description_width': 'initial'},
+                disabled=False,
+                min=0,
+                max=self.electrode_positions.shape[0] - 1,
+            ),
+            widgets.BoundedFloatText(
+                value=0,
+                description='Requested electrode spacing:',
+                style={'description_width': 'initial'},
+                disabled=False,
+                min=0,
+            ),
+            widgets.Button(description='Replace electrodes'),
+        ]
+
+        gbox = GridBox(
+            children=resample_widgets,
+            layout=Layout(
+                width='100%',
+                grid_template_columns='50% 50%',
+                border='solid',
+            )
+        )
+        return resample_widgets, gbox
+
+    def resample_points(self, button):
+        start_electrode = self.resample_widgets[2].value
+        end_electrode = self.resample_widgets[3].value
+        if end_electrode <= start_electrode:
+            print('ERROR: end electrode must be larger than start electrode')
+            return
+
+        nr_electrodes = self.resample_widgets[4].value
+        if nr_electrodes <= 0:
+            print('ERROR: requested electrode spacing must be > 0 m!')
+            return
+
+        el_ids = range(start_electrode, end_electrode + 1)
+        # def get_resampled_positions(data_x_raw, data_z_raw,
+        # requested_spacing):
+        actives = np.where(self.electrode_positions[:, 3])
+        active_els = self.electrode_positions[actives, 0:3].squeeze()
+
+        data_x = active_els[el_ids, 0]
+        data_z = active_els[el_ids, 2]
+        print('number of electrodes:', nr_electrodes)
+        new_x, new_z, N = get_resampled_positions(
+            data_x, data_z, nr_electrodes
+        )
+
+        print('data_x')
+        print(data_x)
+        print('data_z')
+        print(data_z)
+
+        print('new positions:')
+        print(new_x)
+        print(new_z)
+
+        print('----------_')
+
+        # splice the new electrode in
+        self.electrode_positions = np.vstack((
+            self.electrode_positions[0:actives[0][start_electrode], :],
+            np.vstack((
+                new_x,
+                np.ones_like(new_x),
+                new_z,
+                np.ones_like(new_x),
+            )).T,
+            self.electrode_positions[actives[0][end_electrode]+1:, :]
+        ))
+
+        while len(self.el_widgets) < self.electrode_positions.shape[0]:
+            # add new widget rows
+            row = self._get_electrode_widgets_row()
+            self.el_widgets += [row]
+            self.widgets['gridbox'].children += tuple(row)
+
+        # update max-nrs
+        self.resample_widgets[2].max = self.electrode_positions.shape[0] - 2
+        self.resample_widgets[3].max = self.electrode_positions.shape[0] - 1
+        self._update_widgets()
+
     def set_status_use_as_electrode(self, index, change):
         self.electrode_positions[index, 3] = int(change['new'])
         self._update_widgets()
@@ -56,24 +248,28 @@ class electrode_manager(object):
             )
         )
 
+    def _get_electrode_widgets_row(self):
+        widget_row = [
+            widgets.Label('_'),
+            widgets.Label('x'),
+            widgets.Label('z'),
+            widgets.Button(description='Move down'),
+            widgets.Button(description='Move up'),
+            widgets.Checkbox(
+                value=True,
+                description='Use as electrode',
+                disabled=False,
+                indent=False
+            ),
+        ]
+        return widget_row
+
     def _build_widgets(self):
         el_widgets = []
 
         for index, electrode in enumerate(self.electrode_positions):
-            items = []
-            items += [
-                widgets.Label('_'),
-                widgets.Label('x'),
-                widgets.Label('z'),
-                widgets.Button(description='Move down'),
-                widgets.Button(description='Move up'),
-                widgets.Checkbox(
-                    value=True,
-                    description='Use as electrode',
-                    disabled=False,
-                    indent=False
-                ),
-            ]
+            # items = []
+            items = self._get_electrode_widgets_row()
 
             items[3].on_click(
                 lambda x, eindex=index: self.move_down(x, eindex))
@@ -82,7 +278,8 @@ class electrode_manager(object):
 
             items[5].observe(
                 lambda change, eindex=index: self.set_status_use_as_electrode(
-                    eindex, change), names='value'
+                    eindex, change),
+                names='value'
             )
 
             el_widgets += [items]
@@ -94,6 +291,7 @@ class electrode_manager(object):
 
         self.widgets['button_print'] = widgets.Button(
             description='Print Electrode Coordinates',
+            style={'description_width': 'initial'},
             disabled=False,
         )
         self.widgets['output_print'] = widgets.Output()
@@ -148,6 +346,11 @@ class electrode_manager(object):
             self.widgets['button_interp'],
         ])
 
+        self.resample_widgets, resample_gbox = self.get_resampler_widgets()
+        self.resample_widgets[5].on_click(
+            self.resample_points
+        )
+
         self.xz_header = [
             widgets.HTML('<b>El-Nr (1:)</b>'),
             widgets.HTML('<b>x [m]</b>'),
@@ -182,6 +385,7 @@ class electrode_manager(object):
         vbox = widgets.VBox([
             self.widgets['gridbox'],
             self.widgets['hbox_interp'],
+            resample_gbox,
             self.widgets['output_points'],
             self.widgets['button_print'],
             self.widgets['output_print'],
